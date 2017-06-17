1b5e605a4fcb5334170a067d2fd96a5d4fbe7e31
artyn <arty_nuts@hotmail.com>
2017-06-17 11:13:08 -0700

    Create Hello World application
    Create my first hello world application.
    webserver.js: 
    - Requires http module
    - Create a webserver that responds with a html page containing "hello world"
    - Listen on port 80
    
    To test the application
    1. Install node.js
    2. From node.js command prompt run "node webserver.js"

---
+++ webserver.js
@@ -0,0 +1,8 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {  
+  response.writeHead(200, {"Content-Type": "text/html"});  
+  response.write("<!DOCTYPE "html">");  
+  response.write("<html><head><title>My first Node.js webapp</title></head><body>Hello World!</body></html>");  
+  respond.end();});
+server.listen(80);
+console.log("Server is listening");
