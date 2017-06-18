afa4e84c7aba20c34eac70a7b1b54781a7b941a7
artyn <arty_nuts@hotmail.com>
2017-06-17 22:52:07 -0700

    Fix CR feedback. Change " to ' in webserver.js

--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var server = http.createServer(function(request, response) {  
   response.writeHead(200, {"Content-Type": "text/html"});  
-  response.write("<!DOCTYPE "html">");  
+  response.write("<!DOCTYPE 'html'>");  
   response.write("<html><head><title>My first Node.js webapp</title></head><body>Hello World!</body></html>");  
   respond.end();});
 server.listen(80);
